29c0d95a767a8a84c1caa8c0e5827f7eff100b77
Kiran <kiran@gmail.com>
2025-05-18 15:21:35 +0530

    second commit

---
+++ src/main/java/com/java/jenkins/example0/docker_jenkins_integration_sample/Test.java
@@ -0,0 +1,9 @@
+package com.java.jenkins.example0.docker_jenkins_integration_sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+         System.out.println("Test.main()");
+         
+	}
+}
