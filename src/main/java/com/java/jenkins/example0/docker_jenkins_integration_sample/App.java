4ad6aa3940cfcb640c01089d941b712d76bca2e2
Kiran <kiran@gmail.com>
2025-05-18 14:10:22 +0530

    first commit

---
+++ src/main/java/com/java/jenkins/example0/docker_jenkins_integration_sample/App.java
@@ -0,0 +1,13 @@
+package com.java.jenkins.example0.docker_jenkins_integration_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
